6b7c040e6f209cb633b9ff65e636a9a5954d926b
legacyaiapp1199-ship-it <legacyai.app1199@gmail.com>
2026-05-20 13:55:06 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,18 @@
+import { initializeApp } from "firebase/app";
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
+const firebaseConfig = {
+  apiKey: "AIzaSyAcBd8zyTt8BhWxYbYJgC5nO5jfSQcEWLo",
+  authDomain: "legacyai-34c5a.firebaseapp.com",
+  projectId: "legacyai-34c5a",
+  storageBucket: "legacyai-34c5a.firebasestorage.app",
+  messagingSenderId: "307910083498",
+  appId: "1:307910083498:web:82b7484d5805b49324a1d3"
+};
 
+initializeApp(firebaseConfig);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
