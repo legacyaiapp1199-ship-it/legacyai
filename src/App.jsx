07f5ec4b57d155d4a0376571c07c741a7656568a
legacyaiapp1199-ship-it <legacyai.app1199@gmail.com>
2026-05-20 14:18:35 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,8 @@ const handleSignup = async () => {
 
           <button
   onClick={handleSignup}
+  <button
+  onClick={handleSignup}
   className="px-5 py-2 rounded-xl bg-purple-600 hover:bg-purple-700 transition"
 >
   Sign Up
